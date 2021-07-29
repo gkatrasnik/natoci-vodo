f4d383d979f2d152b2bfc9e2f0e74d0645da3a31
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-07-29 15:15:34 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  BottomNavigation,
+  Fab,
+  BottomNavigationAction,
+} from "@material-ui/core";
+import { Add, Restore, LocationOn, Folder } from "@material-ui/icons";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Fab className="addLocation">
+        <Add />
+      </Fab>
+      <BottomNavigation>
+        <BottomNavigationAction
+          label="Recents"
+          value="recents"
+          icon={<Restore />}
+        />
+        <BottomNavigationAction
+          label="Nearby"
+          value="nearby"
+          icon={<LocationOn />}
+        />
+        <BottomNavigationAction
+          label="Folder"
+          value="folder"
+          icon={<Folder />}
+        />
+      </BottomNavigation>
+    </Container>
   );
 }
 
---
+++ src/Components/Geolocation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, BottomNavigation, Fab } from "@material-ui/core";
+import {} from "@material-ui/icons";
+import "./App.css";
+
+function Geolocation() {
+  return null;
+}
+
+export default Geolocation;
